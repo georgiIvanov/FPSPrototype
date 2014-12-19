5e19e46aa0db85a09af1bf36a874ecae23192187
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-12-19 22:04:58 +0200

    projectile class and fire input mapping

---
+++ Source/FPSProject/FPSProjectile.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSProject.h"
+#include "FPSProjectile.h"
+
+AFPSProjectile::AFPSProjectile(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+    CollisionComp = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("SphereComp"));
+    CollisionComp->InitSphereRadius(15.0f);
+    RootComponent = CollisionComp;
+    
+    ProjectileMovement = ObjectInitializer.CreateDefaultSubobject<UProjectileMovementComponent>(this, TEXT("ProjectileComp"));
+    ProjectileMovement->UpdatedComponent = CollisionComp;
+    ProjectileMovement->InitialSpeed = 3000.f;
+    ProjectileMovement->MaxSpeed = 3000.f;
+    ProjectileMovement->bRotationFollowsVelocity = true;
+    ProjectileMovement->bShouldBounce = true;
+    ProjectileMovement->Bounciness = 0.3;
+}
+
+void AFPSProjectile::InitVelocity(const FVector &ShootDirection)
+{
+    if(ProjectileMovement)
+    {
+        // set the projectile's velocity to the desired direction
+        ProjectileMovement->Velocity = ShootDirection * ProjectileMovement->InitialSpeed;
+    }
+}
---
+++ Source/FPSProject/FPSProjectile.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FPSProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSPROJECT_API AFPSProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+    
+    AFPSProjectile(const FObjectInitializer& ObjectInitializer);
+    
+    UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+    class USphereComponent* CollisionComp;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement)
+    class UProjectileMovementComponent* ProjectileMovement;
+    
+    // inits velocity of the projectile in the shoot direction
+    void InitVelocity(const FVector& ShootDirection);
+};
